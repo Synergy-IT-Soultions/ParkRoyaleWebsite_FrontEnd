2913760f03136fe07a1ea61d716d9a656d6b8544
vprasath1902 <venkataprasath.vsa@gmail.com>
2023-04-05 16:57:49 -0400

    Footer Issues Addressed

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -12,10 +12,10 @@ function FooterComponent() {
                         </div>
                     </div>
                     <div className="social-links text-center text-md-right pt-3 pt-md-0">
-                        <a href="https://wa.me/919344539490" className="whatsapp"><i className="bx bxl-whatsapp"></i></a>
-                        <a href="https://wa.me/919344539490" className="whatsapp"><i className="bx bxl-whatsapp"></i></a>
-                        <a href="https://www.facebook.com/people/Park-Royale/100070181315115" className="facebook"><i className="bx bxl-facebook"></i></a>
-                        <a href="https://www.instagram.com/parkroyalekodaikanal/" className="instagram"><i className="bx bxl-instagram"></i></a>
+                        <a href="https://wa.me/919154546666" className="whatsapp" target="_blank"><i className="bx bxl-whatsapp"></i></a>
+                        <a href="https://wa.me/919444432828" className="whatsapp" target="_blank"><i className="bx bxl-whatsapp"></i></a>
+                        <a href="https://www.facebook.com/people/Park-Royale/100070181315115" className="facebook" target="_blank"><i className="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/parkroyalekodaikanal/" className="instagram" target="_blank"><i className="bx bxl-instagram"></i></a>
                     </div>
                 </div>
 
